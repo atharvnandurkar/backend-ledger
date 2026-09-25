718e99c554d24744a05c72f0300ce3374eec6d70
Atharva Nandurkar <devatharv@gmail.com>
2026-09-25 10:53:22 +0530

    new transaction: Validate request

---
+++ src/controllers/transaction.controller.js
@@ -0,0 +1,52 @@
+const transactionModel = require('../models/transaction.model');
+const ledgerModel = require('../models/ledger.model');
+const accountModel = require('../models/account.model');
+const emailService = require('../services/email.service');
+
+/**
+ * - Create a new transaction
+ * THE 10-STEP TRANSFER FLOW: 
+    * 1. Validate request
+    * 2. Validate idemotency key
+    * 3. Check account status
+    * 4. Derive sender balance from ledger
+    * 5. Create transaction (PENDING)
+    * 6. Create DEBIT ledger entry
+    * 7. Create CREDIT ledger entry
+    * 8. Mark transaction COMPLETED
+    * 9. Commit MongoDB Session
+    * 10. Send email notification  
+ */
+
+async function createTransaction(req, res) {
+
+
+    /**
+     * 1. Validate request
+     */
+    const { fromAccount, toAccount, amount, idempotencyKey } = req.body;
+
+    if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
+        return res.status(400).json({
+            message: "FromAccount, toAccount, amount and idempotencyKey are required"
+        })
+    }
+
+    // Check is account is Exist OR Not.
+    const fromUserAccount = await accountModel.findOne({
+        _id: fromAccount,
+    })
+
+    const toUserAccount = await accountModel.findOne({
+        _id: toAccount,
+    })
+
+    if(!fromUserAccount || !toUserAccount) {
+        return res.status(400).json({
+            message: "invalid fromAccount or toAccount"
+        })
+    }
+
+
+    
+}
