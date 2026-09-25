0fcc0aad07d48015166c7c4513c3546626ea9c72
Atharva Nandurkar <devatharv@gmail.com>
2026-09-25 11:05:25 +0530

    create transaction: validate idempotency key

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -41,12 +41,45 @@ async function createTransaction(req, res) {
         _id: toAccount,
     })
 
-    if(!fromUserAccount || !toUserAccount) {
+    if (!fromUserAccount || !toUserAccount) {
         return res.status(400).json({
             message: "invalid fromAccount or toAccount"
         })
     }
 
+    /**
+     * 2. Validate idempotency Key
+     */
 
-    
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey: idempotencyKey
+    })
+
+    if (isTransactionAlreadyExists) {
+        if (isTransactionAlreadyExists.status === "COMPLETED") {
+            res.status(200).json({
+                message: "Transaction already processed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "PENDING") {
+            res.status(200).json({
+                message: "Transaction is still processing"
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "FAILED") {
+            res.status(500).json({
+                message: "Transaction processing failed, please retry"
+            })
+        }
+
+        if (isTransactionAlreadyExists === "REVERSED") {
+            res.status(500).json({
+                message: "Transaction was reversed, please retry"
+            })
+        }
+    }
+
 }
